a2fca4fb364291d51eeb0e1a503dcdc6aaa9591a
patryk <leonidas1123@gmail.com>
2023-09-28 08:15:40 +0200

    Update script for storage

--- scripts/bash/check_storage.sh
+++ scripts/bash/check_storage.sh
@@ -1,66 +1,52 @@
 #!/bin/bash
 
-#This check verify volumes storage. 
+#This check verify volumes storage.
 #Output is generate for each of volume.
+#Created by: Patryk Basko
 
-#check how many "backups" directory are exist
+
+#---------------
+#General variables
+ts=$(date +%s)
+
+
+#---------------
+#check how many "backups" directories are exist.
+#This is main statement for script.
 let count=$(ls /backup/ | wc -l)
-let x=1
+let x=1                                                                                 #global variable, do not modify
 
-#volumes="/backup/backup0$x/bareos"
-
-
-#check how many "devices" are exist
-#let z=1
-#let count_dev=$(ls $volumes | wc -l)
-
+#---------------
+#Check how many "devices" are exist.
+#On this step, loop checking how many "devices" directories exists on backup path.
+#Next create arrays for each of "devices" and filled.
 for (( i=1; i<=$count; i++ )); do
-	volumes="/backup/backup0$x/bareos"
-	let z=1
-	let count_dev=$(ls $volumes | wc -l)
-	#let "x+=1"
-	#for (( y=1; y<=$count_dev; y++ )); do
-	declare -a "volumes_array$x=($(cd $volumes && du -sh * | sed -n '1,5p'))"
-	echo "volumes_array$x" >> /root/tmp.list
-	let "z+=1"
-	let "x+=1"
-	#done
+        volumes="/backup/backup0$x/bareos"
+        let z=1
+        let count_dev=$(ls $volumes | wc -l)
+        #let "x+=1"
+        #for (( y=1; y<=$count_dev; y++ )); do
+        declare -a "volumes_array$x=($(cd $volumes && du -sh * | sed -n '1,5p'))"
+        let "z+=1"
+        let "x+=1"
+        #done
 done
 
-#for each backup directory check storage
+#test space - for debuging. This is for me and should be removed.
+#echo -e "Spis storage oraz zajete miejsce."
+#echo "${volumes_array1[*]}"
+#echo "${volumes_array2[*]}"
 
-#for (( i=1; i<=$count; i++ )); do
-	#volumes="/backup/backup0$x/bareos"#
-#	declare -a "volumes_array$x=($(ls $volumes | du -sh * | sed -n 1p ))"
-#	let "x+=1"
-#done
+#echo -e "========+++++============\nAUTOMAT"
+#echo ""
 
-#let "x-=1"
+#---------------
+#Print info from each of array included output for check_mk.
+#This value is increase depends on arrays count.
+let array_num=1                                                                         #global variable, do not modify
 
-#echo "Koniec pierwszej petli, x=$x"
-#for earch main directory, print included storages
-
-echo -e "Spis storage oraz zajete miejsce."
-echo "${volumes_array1[*]}"
-echo "${volumes_array2[*]}"
-
-echo "AUTOMAT"
-echo ""
-
-while read -r line
-do
-	echo $line
-done < /root/tmp.list
-
-rm /root/tmp.list
-#z=x
-#let x=1
-#read_array="volumes_array$x"
-#for (( i=1; i>=$z; i++ )); do
-#	echo "${read_array[@]}"
-#	let "x+=1"
-#done
-
-#echo -e "${volumes_array1[*]}"
-#echo -e "${volumes_array2[*]}"
-#echo -e "${volumes_array3[*]}"
+for (( i=1; i<=$x; i++ )); do
+        array_name=volumes_array$array_num[@]
+        printf "P "check_storage" |storage=%s;20;50 volume=%s \n" ${!array_name}
+        let "array_num+=1"
+done
